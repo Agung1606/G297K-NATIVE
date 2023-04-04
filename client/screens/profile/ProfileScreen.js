e4cd76bb9dbabce5152ffee13ab8e360a6882a5b
agungsaputra <agngsptra928@gmail.com>
2023-04-04 14:34:53 +0700

    start working on profile UI but it still experiment

--- client/screens/profile/ProfileScreen.js
+++ client/screens/profile/ProfileScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Pressable } from 'react-native'
+import { Avatar } from 'react-native-paper';
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -29,16 +30,65 @@ export default function ProfileScreen({ route }) {
   const userId = route?.params?.param;
   const token = useSelector((state) => state.auth.token);
   // data
-  const { data, loading } = useQuery(GET_USER, {
+  const { data } = useQuery(GET_USER, {
     variables: {
       token: token,
       userId: userId
     }
   });
+
+  // USER INFORMATION
+  const fullname = `${data?.getUser?.firstName} ${data?.getUser?.lastName}`;
   
   return (
-    <SafeAreaView>
-      
+    <SafeAreaView className='flex-1 bg-white'>
+      <View className='mx-3'>
+        <Text>close button</Text>
+      </View>
+      {/* wrapper */}
+      <View 
+        className='bg-[#d4d4d4] h-[180px] px-[10px] mx-2 mt-6 rounded-xl
+          flex-row justify-between items-center'
+      >
+        {/* picture path and username */}
+        <View className='justify-center items-center gap-y-2'>
+          <Avatar.Image 
+            size={95} 
+            source={{uri: `http://192.168.0.106:6002/assets/${data?.getUser?.profilePicturePath}`}} 
+          />
+          <Text className='font-bold'>{fullname}</Text>
+        </View>
+        {/* wrapper info */}
+        <View className='space-y-4'>
+          {/* posts, followers, following */}
+          <View className='flex-row items-center gap-x-[15px]'>
+            <View className='justify-center items-center'>
+              <Text className='text-lg font-bold'>
+                16
+              </Text>
+              <Text>Posts</Text>
+            </View>
+            <View className='justify-center items-center'>
+              <Text className='text-lg font-bold'>
+                {data.getUser.followers}
+              </Text>
+              <Text>Followers</Text>
+            </View>
+            <View className='justify-center items-center'>
+              <Text className='text-lg font-bold'>
+                {data.getUser.following}
+              </Text>
+              <Text>Following</Text>
+            </View>
+          </View>
+          {/* button */}
+          <Pressable className='bg-deep-blue rounded-lg'>
+            <Text className='text-center text-lg font-semibold py-[2px]'>
+              Edit profile
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
